5621f3785f9d065eda92c1c91ef9531c1b114efa
JhonPalominoC <142623871+JhonPalominoC@users.noreply.github.com>
2023-09-19 23:39:53 -0500

    Add files via upload

---
+++ src/laboratorio05/Laboratorio05.java
@@ -0,0 +1,21 @@
+
+
+package laboratorio05;
+
+/**
+ *
+ * @author emers
+ */
+public class Laboratorio05 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        FrameEstudiantes pantalla = new FrameEstudiantes();
+        pantalla.setLocationRelativeTo(null);
+        pantalla.setVisible(true);
+    }
+    
+}
